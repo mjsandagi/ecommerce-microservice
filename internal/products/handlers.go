fe61df68cb64eba8a95955e603a266c1c3f035ac
mjsandagi <mjsandagi@users.noreply.github.com>
2025-12-15 22:28:07 +0000

    Fallback to previous handling function

--- internal/products/handlers.go
+++ internal/products/handlers.go
@@ -1,7 +1,8 @@
 package products
 
 import "net/http"
-import "encoding/json"
+import "github.com/mjsandagi/go-ecommerce/internal/json"
+// import "log"
 
 type handler struct {
 	service Service
@@ -16,7 +17,12 @@ func NewHandler(service Service) *handler {
 
 func (h *handler) ListProducts(w http.ResponseWriter, r *http.Request) {
 	// 1. Call the service layer --> ListProducts method
+	// err := h.service.ListProducts(r.Context())
+	// if err != nil {
+	// 	log.Println("Error fetching products:", err)
+	// 	http.Error(w, error.Error(err), http.StatusInternalServerError) // The server encountered an unexpected error, preventing it from fulfilling the request.
+	// }
 	// 2. Return JSON response
 	products := []string{"Product 1", "Product 2", "Product 3"}
-	json.NewEncoder(w).Encode(products)
+	json.Write(w, http.StatusOK, products)
 }
