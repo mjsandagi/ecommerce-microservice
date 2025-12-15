0e368e83c1757859aeb2361774d92f2486bf5611
mjsandagi <mjsandagi@users.noreply.github.com>
2025-12-15 22:28:21 +0000

    Added services

--- internal/products/service.go
+++ internal/products/service.go
@@ -1,5 +1,21 @@
 package products
 
+import "context"
+
 type Service interface {
 	// Add fields as necessary, e.g., a reference to the service layer
+	ListProducts(ctx context.Context) error
+}
+
+type svc struct {
+	// repository
+}
+
+func NewService() Service {
+	return &svc{
+	}
+}
+
+func (s *svc) ListProducts(ctx context.Context) (error) {
+	return nil
 }
